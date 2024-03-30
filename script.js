ebfa7244d2e1eddbd8c088ee0e3cac833def8354
Marvin Idibia <midibia400@gmail.com>
2024-03-30 04:47:07 +0000

    update

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let body = document.getElementById('body');
 let themeChanger = document.getElementById('themeButton');
 let themeMode = document.getElementById('themeMode');
 let themeBack = document.getElementById('button-background');
+let barMenu = document.getElementById('barMenu');
+let iconContainer = document.getElementById('iconContainer');
+let currentWebWidth = document.documentElement.clientWidth
 
-
+console.log(currentWebWidth)
 function changeTheme() {
     if(body.classList.contains('body1')){
         body.classList.remove('body1');
@@ -25,6 +28,32 @@ function changeTheme() {
     }
 }
 
-function openOption() {
-    let
+
+window.addEventListener('resize', function(){
+    if(window.innerWidth > 630){
+        if(barMenu.classList.contains('menuIsActive') || iconContainer.style.color =='transparent' ){
+            barMenu.classList.remove('menuIsActive');
+            barMenu.classList.add('barMenu');
+            iconContainer.style.color = '#fff';
+        }
+    }
+    // else if(window.innerWidth <= 630){
+    //     if(barMenu.classList.contains('barMenu')){
+    //         barMenu.classList.add('menuIsActive');
+    //         barMenu.classList.remove('barMenu')
+    //     }
+    // }
+})
+
+
+function openOptions() {
+    if(currentWebWidth <= 630){
+        iconContainer.style.color = 'transparent';
+        barMenu.classList.remove('barMenu');
+        barMenu.classList.add('menuIsActive');
+    }
 }
+
+// transition: all 500ms ease-in-out;
+
+

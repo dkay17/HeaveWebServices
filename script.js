a9a39cafe1b250a3742e643e6b2a7f8dd5c2183c
Marvin Idibia <midibia400@gmail.com>
2024-03-31 00:13:25 +0000

    update

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let themeBack = document.getElementById('button-background');
 let barMenu = document.getElementById('barMenu');
 let iconContainer = document.getElementById('iconContainer');
 let icon = document.getElementById('fa-solid')
+let closeMen = document.getElementById('closeMenu')
 let currentWebWidth = document.documentElement.clientWidth
 
 console.log(currentWebWidth)
 function changeTheme() {
+    // Checks if the current theme is in dark mode and changes to light mode
     if(body.classList.contains('body1')){
         body.classList.remove('body1');
         body.classList.add('body2');
@@ -17,7 +19,13 @@ function changeTheme() {
         themeChanger.classList.remove('themeButton');
         themeChanger.classList.add('but2');
         themeMode.innerHTML = 'Light mode';
+        icon.style.color = '#fff';
+        if(icon.classList.contains('fa-moon')){
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-bars');
+        }
     }
+    // Checks if the current theme is in loght mode and changes to dark mode
     else if(body.classList.contains('body2')){
         body.classList.remove('body2');
         body.classList.add('body1');
@@ -26,10 +34,15 @@ function changeTheme() {
         themeChanger.classList.remove('but2');
         themeChanger.classList.add('themeButton');
         themeMode.innerHTML = 'Dark mode';
+        icon.style.color = 'black';
+        if(icon.classList.contains('fa-bars')){
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-moon');
+        }
     }
 }
 
-
+// This code is incomplete(what's left: color change when theme is changed)
 window.addEventListener('resize', function(){
     if(window.innerWidth > 630){
         if(barMenu.classList.contains('menuIsActive') || iconContainer.style.color =='transparent' ){
@@ -37,13 +50,14 @@ window.addEventListener('resize', function(){
             barMenu.classList.add('barMenu');
             iconContainer.style.color = '#fff';
         }
+
+        window.addEventListener('resize', function(){
+            if(window.innerWidth <= 630){
+                this.location.reload();
+            }
+        })
     }
-    // else if(window.innerWidth <= 630){
-    //     if(barMenu.classList.contains('barMenu')){
-    //         barMenu.classList.add('menuIsActive');
-    //         barMenu.classList.remove('barMenu')
-    //     }
-    // }
+    // this.location.reload()
 })
 
 
@@ -52,6 +66,15 @@ function openOptions() {
         iconContainer.style.color = 'transparent';
         barMenu.classList.remove('barMenu');
         barMenu.classList.add('menuIsActive');
+    }
+}
+
+// This code is incomplete(what's left: color change when theme is changed)
+function closeMenu() {
+    if(currentWebWidth <= 630){
+        barMenu.classList.remove('menuIsActive');
+        barMenu.classList.add('barMenu');
+        iconContainer.style.color = '#fff';
     }
 }
 
